d8d88cf2351df9e738eb62e18c01786bd0718a9d
云龙 李 <oimscient@outlook.com>
2022-06-15 16:40:59 +0800

    添加注释

--- LocalData/Data/CountMonths.cs
+++ LocalData/Data/CountMonths.cs
@@ -42,15 +42,13 @@ namespace LocalData.Data
             Timer1.Enabled = true;
         }
 
-
-
         /// <summary>
         /// 全月统计
         /// </summary>
         /// <param name="date">日期（Y-M-d）</param>
         public void CountMonth(string date)
         {
-            //获取车辆
+            //获取全月车辆数据（车辆编号、车辆类型、运输总和、非正常运输总和、燃油消耗总和、加油总和、里程总和、车次总和、平均满载率、油耗异常总数、超速异常总数、非正常运输总数）
             string sql = "SELECT VEHICLE_ID as id,TYPE as type,SUM(WEIGHT) as weight ,SUM(UNUSUAL_WEIGHT) as unu_weight,SUM(USE_FUEL) as fule,SUM(INJECT_FUEL) as inject,SUM(MILEAGE) as mileage,SUM(LOAD_NUM) as num,AVG(LOAD_FACTOR) as factor,SUM(UNFUEL) as unfuel,SUM(UNSPEED) as unspeed, SUM(UNTRANS) as untrans from count_sys_day where  COMPANY='" + Company + "' and DATE_FORMAT(ADD_TIME,'%Y-%m')=DATE_FORMAT('" + date + "','%Y-%m') GROUP BY VEHICLE_ID";
             List<Dictionary<string, string>> list = mysql.MultipleSelect(sql, new List<string>() { "id", "type", "weight", "unu_weight", "fule", "inject", "mileage", "num", "factor", "unfuel", "unspeed", "untrans" });
             try
